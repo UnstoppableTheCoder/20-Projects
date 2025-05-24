a3675f4e5858919ad62a788a4c49ebc35a12b7a0
Vipendra Singh Patel <122035117+UnstoppableTheCoder@users.noreply.github.com>
2025-05-24 11:30:08 +0530

    Update vite.config.js

--- MERN-CRASH-COURSE/frontend/vite.config.js
+++ MERN-CRASH-COURSE/frontend/vite.config.js
@@ -5,11 +5,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: {
-    proxy: {
-      "/api": { // whenever we visit /api, you should prefix with target
-        target: "http://localhost:5000",
-      },
-    },
-  },
+  // server: { // Just for development
+    // proxy: {
+    //   "/api": { // whenever we visit /api, you should prefix with target
+    //     target: "http://localhost:5000",
+    //   },
+    // },
+  // },
 });

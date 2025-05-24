85833f2c7cada6cd96fda86b3b125373a89e43e6
Unstoppable <VipendraSinghPatel999@gmail.com>
2025-05-24 11:05:48 +0530

    Add Mern-Crash-Course Project

---
+++ MERN-CRASH-COURSE/frontend/src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
+import React from "react";
+import { CiSquarePlus } from "react-icons/ci";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+import { Link } from "react-router-dom";
+import { useColorMode } from "./ui/color-mode";
+
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDirection={{ base: "column", sm: "row" }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"to-r"}
+          gradientFrom={"cyan.400"}
+          gradientTo={"blue.500"}
+          bgClip={"text"}
+        >
+          <Link to={"/"}>Product Store 🛒</Link>
+        </Text>
+
+        <HStack spaceX={2} spaceY={2} alignItems={"center"}>
+          <Flex gap={2}>
+            <Link to={"/create"}>
+              <Button>
+                <CiSquarePlus fontSize={20} />
+              </Button>
+            </Link>
+
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <IoMoon /> : <LuSun />}
+            </Button>
+          </Flex>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
